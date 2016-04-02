377cd6045d0f26446f586e3a03ce36fc1a0a77ff
lesleyxxx12 <lesleyvanderlee318@gmail.com>
2016-04-03 01:05:48 +0200

    GPU Overclock @ 192 MHz

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -949,6 +949,12 @@ static struct resource kgsl_3d0_resources[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
+		//Fake freq to avoid kernel errors
+	        {
+                        .gpu_freq = 280000000,
+                        .bus_freq = 220000000,
+                },
+		//End
 		{
 			.gpu_freq = 245760000,
 			.bus_freq = 200000000,
@@ -963,7 +969,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 0,
-	.num_levels = 3,
+	.num_levels = 4,
 	.set_grp_async = set_grp_xbar_async,
 	.idle_timeout = HZ,
 	.strtstp_sleepwake = true,
@@ -984,11 +990,13 @@ struct platform_device msm_kgsl_3d0 = {
 void __init msm7x25a_kgsl_3d0_init(void)
 {
 	if (cpu_is_msm7x25a() || cpu_is_msm7x25aa() || cpu_is_msm7x25ab()) {
-		kgsl_3d0_pdata.num_levels = 2;
-		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 133330000;
-		kgsl_3d0_pdata.pwrlevel[0].bus_freq = 160000000;
-		kgsl_3d0_pdata.pwrlevel[1].gpu_freq = 96000000;
-		kgsl_3d0_pdata.pwrlevel[1].bus_freq = 0;
+		kgsl_3d0_pdata.num_levels = 3;
+		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 192000000;
+  		kgsl_3d0_pdata.pwrlevel[0].bus_freq = 192000000;
+		kgsl_3d0_pdata.pwrlevel[1].gpu_freq = 133330000;
+		kgsl_3d0_pdata.pwrlevel[1].bus_freq = 160000000;
+		kgsl_3d0_pdata.pwrlevel[2].gpu_freq = 96000000;
+		kgsl_3d0_pdata.pwrlevel[2].bus_freq = 0;
 	}
 }
 
